7234a67bbea48012dccb75043a11f68b0ec1dc05
AllaLoginova <122536827+AllaLoginova@users.noreply.github.com>
2024-11-07 08:55:56 +0300

    add new folder

---
+++ 67-68/home_work.py
@@ -0,0 +1,13 @@
+SELECT * FROM products;
+
+SELECT * FROM products WHERE type="Овощ";
+
+SELECT * FROM products WHERE type="Фрукт";
+
+SELECT name FROM products;
+
+SELECT DISTINCT color FROM products;
+
+SELECT * FROM products WHERE type='Фрукт' AND color="Оранжевый";
+
+SELECT * FROM products WHERE type='Овощ' AND color="Зеленый";
